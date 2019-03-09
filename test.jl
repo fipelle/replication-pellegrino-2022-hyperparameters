28715204aefd36d51cc6b6f5203becffe5ed86ae
fipelle <6272230+fipelle@users.noreply.github.com>
2019-03-09 22:50:33 +0000

    Fixed bug in fc_err

--- test.jl
+++ test.jl
@@ -16,8 +16,8 @@ p=2;
 α=0.5;
 β=1.3;
 
-#@time iis_loss = fc_err(Y_zscored, p, λ, α, β, tol=1e-3, verb=true);
-#@time oos_loss = fc_err(Y_zscored, p, λ, α, β, iis=false, t0=60, tol=1e-3, verb=true);
-bjk_loss = jackknife_err(Y_zscored, p, λ, α, β, ajk=false, t0=60, tol=1e-3);
-#@time ajk_loss = jackknife_err(Y_zscored, p, λ, α, β, ajk=true, t0=60, tol=1e-3, max_samples=200);
+@time iis_loss = fc_err(Y_zscored, p, λ, α, β, iis=true, tol=1e-3, verb=true);
+@time oos_loss = fc_err(Y_zscored, p, λ, α, β, iis=false, t0=60, tol=1e-3, verb=true);
+@time bjk_loss = jackknife_err(Y_zscored, p, λ, α, β, ajk=false, t0=60, tol=1e-3);
+@time ajk_loss = jackknife_err(Y_zscored, p, λ, α, β, ajk=true, t0=60, tol=1e-3, max_samples=200);
 #@time ajk_loss_500 = jackknife_err(Y_zscored, p, λ, α, β, ajk=true, t0=60, tol=1e-3, max_samples=500);
