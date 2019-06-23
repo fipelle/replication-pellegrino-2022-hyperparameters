cb3b27523f57ee85b95df29f4d3ca78ef78252c0
fipelle <6272230+fipelle@users.noreply.github.com>
2019-06-23 17:04:14 +0100

    Add: docstring for kforecast

--- ElasticNetVAR/ElasticNetVAR.jl
+++ ElasticNetVAR/ElasticNetVAR.jl
@@ -140,7 +140,7 @@ module ElasticNetVAR
 	export JVector, JArray, KalmanSettings, KalmanStatus;
 	export mean_skipmissing, std_skipmissing, is_vector_in_matrix, sym, sym_inv, demean, lag, companion_form, ext_companion_form, no_combinations, rand_without_replacement!;
 	export kalman;
-	export kfilter!;
+	export kfilter!, kforecast;
 	export coordinate_descent, ecm;
 	export block_jackknife, artificial_jackknife;
 	export select_hyperparameters, fc_err, jackknife_err;
