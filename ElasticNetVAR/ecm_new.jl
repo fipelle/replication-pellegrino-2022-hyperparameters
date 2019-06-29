7226747e69df78c49c112d8639d25540c3f728db
fipelle <6272230+fipelle@users.noreply.github.com>
2019-06-29 19:03:04 +0100

    Added first draft of ksmoother_ecm

--- ElasticNetVAR/ecm_new.jl
+++ ElasticNetVAR/ecm_new.jl
@@ -4,6 +4,60 @@ envar_penalty(estim_settings::EstimSettings, Σ::SymMatrix, Ψ::FloatArray, Φ::
 envar_penalty_ridge(estim_settings::EstimSettings, Ψ::FloatArray) = (1-estim_settings.α)/2.*Symmetric(Ψ*estim_settings.Γ*Ψ')::SymMatrix;
 envar_penalty_lasso(estim_settings::EstimSettings, Ψ::FloatArray, Φ::FloatArray) = estim_settings.α/2.*Symmetric((Ψ .* sqrt.(Φ))*estim_settings.Γ*(Ψ .* sqrt.(Φ))')::SymMatrix;
 
+
+"""
+"""
+function ksmoother_ecm(estim_settings::EstimSettings, kalman_settings::KalmanSettings, status::KalmanStatus)
+
+    # Memory pre-allocation
+    E = zeros(estim_settings.n, estim_settings.n);
+    F = zeros(estim_settings.n, estim_settings.np);
+    G = zeros(estim_settings.np, estim_settings.np);
+    Xs = copy(status.X_post);
+    Ps = copy(status.P_post);
+
+    # Loop over t
+    for t=status.t:-1:2
+
+        # Pointers
+        Xp = status.history_X_prior[t];
+        Pp = status.history_P_prior[t];
+        Xf_lagged = status.history_X_post[t-1];
+        Pf_lagged = status.history_P_post[t-1];
+
+        # Smoothed estimates for t-1
+        J1 = compute_J1(Pf_lagged, Pp, kalman_settings);
+        Xs_old = backwards_pass(Xf_lagged, J1, Xs, Xp);
+        Ps_old = backwards_pass(Pf_lagged, J1, Ps, Pp);
+
+        # Update ECM statistics
+        E += Xs[1:n]*Xs[1:n]' + Ps[1:n,1:n];
+        F += Xs[1:n]*Xs_old[1:np]' + Ps[1:n,n+1:end];
+        G += Xs_old[1:np]*Xs_old[1:np]' + Ps_old[1:np,1:np];
+
+        # Update Xs and Ps
+        Xs = copy(Xs_old);
+        Ps = copy(Ps_old);
+    end
+
+    # Pointers
+    Xp = status.history_X_prior[1];
+    Pp = status.history_P_prior[1];
+
+    # Compute smoothed estimates for t==0
+    J1 = compute_J1(settings.P0, Pp, settings);
+    X0 = backwards_pass(settings.X0, J1, Xs, Xp);
+    P0 = backwards_pass(settings.P0, J1, Ps, Pp);
+
+    # Update ECM statistics
+    E += Xs[1:n]*Xs[1:n]' + Ps[1:n,1:n];
+    F += Xs[1:n]*X0[1:np]' + Ps[1:n,n+1:end];
+    G += X0[1:np]*X0[1:np]' + P0[1:np,1:np];
+
+    # Return ECM statistics
+    return E, F, G;
+end
+
 """
     ecm(estim_settings::EstimSettings)
 
@@ -59,9 +113,8 @@ function ecm(estim_settings::EstimSettings)
         # Run Kalman filter and smoother
         status = KalmanStatus();
         for t=1:settings.T
-            kfilter!(settings, status);
+            kfilter!(kalman_settings, status);
         end
-        Xs, Ps, X0s, P0s = ksmoother(settings, status); # TBC write new function to get the ecm statistics without saving the smoother history
 
         if iter > prerun
 
@@ -84,23 +137,7 @@ function ecm(estim_settings::EstimSettings)
             verb_message(estim_settings.verb, "ecm > prerun $iter (out of $prerun)");
         end
 
-        # ECM statistics (TBC - use the new ecm smoother function)
-        E = zeros(n, n);
-        F = zeros(n, np);
-        G = zeros(np, np);
-
-        for t=1:T
-            E += Xs[1:n,t]*Xs[1:n,t]' + Ps[1:n,1:n,t];
-
-            if t == 1
-                F += Xs[1:n,t]*X0s[1:np]' + Ps[1:n,n+1:end,t];
-                G += X0s[1:np]*X0s[1:np]' + P0s[1:np,1:np];
-
-            else
-                F += Xs[1:n,t]*Xs[1:np,t-1]' + Ps[1:n,n+1:end,t];
-                G += Xs[1:np,t-1]*Xs[1:np,t-1]' + Ps[1:np,1:np,t-1];
-            end
-        end
+        E, F, G = ksmoother_ecm(estim_settings, kalman_settings, status);
 
         # VAR(p) coefficients
         Φ = 1 ./ (abs.(Ψ).+ε);
