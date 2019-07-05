6c9a1d1cb8fd5e4b38f1f2b759c0ab5621e3237f
fipelle <6272230+fipelle@users.noreply.github.com>
2019-07-05 22:01:51 +0100

    Final first draft for validation_new.jl

--- ElasticNetVAR/validation_new.jl
+++ ElasticNetVAR/validation_new.jl
@@ -1,241 +1,198 @@
 """
-    select_hyperparameters(estim_settings::EstimSettings, validation_settings::ValidationSettings, γ_grid::HyperGrid)
+    select_hyperparameters(validation_settings::ValidationSettings, γ_grid::HyperGrid)
 
 Select the tuning hyper-parameters for the elastic-net vector autoregression.
 
 # Arguments
-- `estim_settings`: EstimSettings struct
 - `validation_settings`: ValidationSettings struct
 - `γ_grid`: HyperGrid struct
 
 # References
 Pellegrino (2019)
 """
-function select_hyperparameters(estim_settings::EstimSettings, validation_settings::ValidationSettings, γ_grid::HyperGrid)
+function select_hyperparameters(validation_settings::ValidationSettings, γ_grid::HyperGrid)
 
     # Check inputs
-    check_bounds(estim_settings.p, 1);
-    check_bounds(estim_settings.λ, 0);
-    check_bounds(estim_settings.α, 0, 1);
-    check_bounds(estim_settings.β, 1);
-    check_bounds(estim_settings.max_iter, 3);
-    check_bounds(estim_settings.max_iter, estim_settings.prerun);
-    check_bounds(estim_settings.n, 2); # It supports only multivariate models (for now ...)
+    check_bounds(validation_settings.max_iter, 3);
+    check_bounds(validation_settings.max_iter, validation_settings.prerun);
+    check_bounds(validation_settings.n, 2); # It supports only multivariate models (for now ...)
 
     if length(γ_grid.p) != 2 || length(γ_grid.λ) != 2 || length(γ_grid.α) != 2 || length(γ_grid.β) != 2
-        error("The grids include more than two entries. Random search algorithm: they must include only the bounds for the grids!")
+        error("The grids include more than two entries. They must include only the lower and upper bounds for the grids!")
     end
 
     check_bounds(γ_grid.p[2], γ_grid.p[1]);
     check_bounds(γ_grid.λ[2], γ_grid.λ[1]);
     check_bounds(γ_grid.α[2], γ_grid.α[1]);
     check_bounds(γ_grid.β[2], γ_grid.β[1]);
-
-    #=
-    DA QUI IN GIU'
-    =#
+    check_bounds(γ_grid.p[1], 1);
+    check_bounds(γ_grid.λ[1], 0);
+    check_bounds(γ_grid.α[1], 0, 1);
+    check_bounds(γ_grid.α[2], 0, 1);
+    check_bounds(γ_grid.β[1], 1);
 
     # Construct grid of hyperparameters - random search algorithm
-    error_grid = zeros(rs_draws);
-    γ_grid = Array{Array{Float64,1}}(UndefInitializer(), rs_draws);
+    errors     = zeros(γ_grid.draws);
+    candidates = zeros(4, γ_grid.draws);
 
-    for draw=1:rs_draws
-        γ_grid[draw] = vcat(rand(p_grid_0[1]:p_grid_0[2]),
-                            rand(Uniform(λ_grid_0[1], λ_grid_0[2])),
-                            rand(Uniform(α_grid_0[1], α_grid_0[2])),
-                            rand(Uniform(β_grid_0[1], β_grid_0[2])));
+    for draw=1:γ_grid.draws
+        candidates[:,draw] = [rand(γ_grid.p[1]:γ_grid.p[2]),
+                              rand(Uniform(γ_grid.λ[1], γ_grid.λ[2])),
+                              rand(Uniform(γ_grid.α[1], γ_grid.α[2])),
+                              rand(Uniform(γ_grid.β[1], γ_grid.β[2]))];
     end
 
-    hyper_grid = zeros(4, length(error_grid));
-
-    open("$log_folder/status.txt", "w") do io
+    open("$(validation_settings.log_folder_path)/status.txt", "w") do io
         write(io, "")
     end
 
-    iter = 1;
-    for γ=γ_grid
+    for iter=1:γ_grid.draws
 
         # Retrieve candidate hyperparameters
-        p, λ, α, β = γ;
+        p, λ, α, β = candidates[:,iter];
         p = Int64(p);
 
         # Update log
-        if verb == true
-            message = "select_hyperparameters (error estimator $err_type) > running iteration $iter (out of $(length(error_grid))), γ=($(round(p,digits=3)), $(round(λ,digits=3)), $(round(α,digits=3)), $(round(β,digits=3)))";
+        if validation_settings.verb == true
+            message = "select_hyperparameters (error estimator $(validation_settings.err_type)) > running iteration $iter (out of $(γ_grid.draws), γ=($(round(p,digits=3)), $(round(λ,digits=3)), $(round(α,digits=3)), $(round(β,digits=3)))";
             println(message);
-            open("$log_folder/status.txt", "a") do io
+            open("$(validation_settings.log_folder_path)/status.txt", "a") do io
                 write(io, "$message\n")
             end
         end
 
-        # In-sample error
-        if err_type == 1
-            error_grid[iter] = fc_err(Y, p, λ, α, β, iis=true, tol=tol, max_iter=max_iter, prerun=prerun, verb=false, demean_Y=demean_Y);
-
-        # Out-of-sample error
-        elseif err_type == 2
-            error_grid[iter] = fc_err(Y, p, λ, α, β, iis=false, t0=t0, tol=tol, max_iter=max_iter, prerun=prerun, verb=false, demean_Y=demean_Y);
-
-        # Artificial jackknife error
-        elseif err_type == 3
-            error_grid[iter] = jackknife_err(Y, p, λ, α, β, ajk=true, subsample=subsample, max_samples=max_samples, t0=t0, tol=tol, max_iter=max_iter, prerun=prerun, verb=verb, demean_Y=demean_Y);
-
-        # Block jackknife error
-        elseif err_type == 4
-            error_grid[iter] = jackknife_err(Y, p, λ, α, β, ajk=false, subsample=subsample, max_samples=max_samples, t0=t0, tol=tol, max_iter=max_iter, prerun=prerun, verb=verb, demean_Y=demean_Y);
+        # Evaluate
+        if validation_settings.err_type < 3
+            errors[iter] = fc_err(validation_settings, p, λ, α, β);
+        else
+            errors[iter] = jackknife_err(validation_settings, p, λ, α, β);
         end
-
-        # Update hyper_grid and iter
-        hyper_grid[:, iter] = [p, λ, α, β];
-        iter += 1;
     end
 
-    if verb == true
-        println("");
-    end
+    verb_message(validation_settings.verb, "");
 
     # Return output
-    ind_min = argmin(error_grid);
-    return hyper_grid[:, ind_min], error_grid, hyper_grid;
+    return candidates, errors;
 end
 
-
 """
-    fc_err(data::JArray{Float64,2}, p::Int64, λ::Number, α::Number, β::Number; iis::Bool=false, t0::Int64=1, tol::Float64=1e-3, max_iter::Int64=1000, prerun::Int64=2, verb::Bool=true, demean_Y::Bool=true)
+    fc_err(validation_settings::ValidationSettings, p::Int64, λ::Number, α::Number, β::Number)
 
-Return the in-sample / out-of-sample error.
+Compute the in-sample / out-of-sample error associated with the candidate hyperparameters
 
 # Arguments
-- `data`: observed measurements (`nxT`), where `n` and `T` are the number of series and observations.
-- `p`: number of lags in the vector autoregression
-- `λ`: overall shrinkage hyper-parameter for the elastic-net penalty
-- `α`: weight associated to the LASSO component of the elastic-net penalty
-- `β`: additional shrinkage for distant lags (p>1)
-- `iis`: True (false) for the in-sample (out-of-sample) error (default: false)
-- `t0`: End of the estimation sample (default: 1)
-- `tol`: tolerance used to check convergence (default: 1e-3)
-- `max_iter`: maximum number of iterations for the estimation algorithm (default: 1000)
-- `prerun`: number of iterations prior the actual ECM estimation routine (default: 2)
-- `verb`: Verbose output (default: true)
-- `demean_Y`: demean data (default: true)
+- `validation_settings`: ValidationSettings struct
+- `p`: (candidate) number of lags in the vector autoregression
+- `λ`: (candidate) overall shrinkage hyper-parameter for the elastic-net penalty
+- `α`: (candidate) weight associated to the LASSO component of the elastic-net penalty
+- `β`: (candidate) additional shrinkage for distant lags (p>1)
+- `jth_jackknife_data`: j-th jackknife sample (default: nothing)
 
 # References
 Pellegrino (2019)
 """
-function fc_err(data::JArray{Float64,2}, p::Int64, λ::Number, α::Number, β::Number; iis::Bool=false, t0::Int64=1, tol::Float64=1e-3, max_iter::Int64=1000, prerun::Int64=2, verb::Bool=true, demean_Y::Bool=true)
+function fc_err(validation_settings::ValidationSettings, p::Int64, λ::Number, α::Number, β::Number; jth_jackknife_data::Union{JArray{Float64}, Nothing}=nothing)
 
-    # Initialise
-    n, T = size(data);
+    t0 = validation_settings.T;
 
-    # In-sample
-    if iis == true
-
-        # Demean data
-        if demean_Y == true
-            Y = demean(data) |> JArray{Float64};
-        else
-            Y = copy(data) |> JArray{Float64};
-        end
-
-        # Estimate the penalised VAR
-        B̂, R̂, Ĉ, V̂, 𝔛0̂, P0̂, _, _ = ecm(Y, p, λ, α, β, tol=tol, max_iter=max_iter, prerun=prerun, verb=verb);
-
-        # Run Kalman filter and smoother
-        _, _, _, _, _, _, 𝔛p, _, _ = kalman(Y, B̂, R̂, Ĉ, V̂, 𝔛0̂, P0̂; loglik_flag=false, kf_only_flag=true);
-
-        # Residuals
-        resid = (𝔛p[1:size(Y,1), :] - Y).^2 |> JArray{Float64};
-        ind_not_all_missings = sum(ismissing.(Y), dims=1) .!= size(Y,1);
-
-    # Out-of-sample
-    else
-
-        # Run Kalman filter and smoother
-        𝔛p = zeros(n, T-t0);
-
-        if demean_Y == true
-            Y = data.-mean_skipmissing(data[:,1:t0]) |> JArray{Float64};
-        else
-            Y = data[:,1:t] |> JArray{Float64};
-        end
-
-        # Estimate the penalised VAR
-        B̂, R̂, Ĉ, V̂, 𝔛0̂, P0̂, _, _ = ecm(Y[:,1:t0], p, λ, α, β, tol=tol, max_iter=max_iter, prerun=prerun, verb=verb);
-
-        # Out-of-sample
-        _, _, _, _, _, _, 𝔛p_t, _, _ = kalman(Y, B̂, R̂, Ĉ, V̂, 𝔛0̂, P0̂; loglik_flag=false, kf_only_flag=true);
-
-        # Store new forecast
-        𝔛p .= 𝔛p_t[1:n, t0+1:end];
-
-        # Residuals
-        resid = (𝔛p - Y[:, t0+1:end]).^2 |> JArray{Float64};
-        ind_not_all_missings = sum(ismissing.(Y[:, t0+1:end]), dims=1) .!= size(Y,1);
+    # Out-of-sample error
+    if validation_settings.err_type != 1
+        t0 = validation_settings.t0;
     end
 
-    # Removes t with missings only
-    ind_not_all_missings = findall(ind_not_all_missings[:]);
-    resid = resid[:, ind_not_all_missings];
+    # Jackknife out-of-sample
+    if validation_settings.err_type > 2
+        μ = mean(jth_jackknife_data[:, 1:t0], dims=2);
+        σ = std(jth_jackknife_data[:, 1:t0], dims=2);
+        Y = @. jth_jackknife_data[:, 1:t0] - μ;
+        Y_output = @. jth_jackknife_data - μ;
 
-    # Compute loss
-    loss = mean([mean_skipmissing(resid[:,t]) for t=1:size(resid,2)]);
+    # Standard out-of-sample
+    else
+        μ = mean(validation_settings.Y[:, 1:t0], dims=2);
+        σ = std(validation_settings.Y[:, 1:t0], dims=2);
+        Y = @. validation_settings.Y[:, 1:t0] - μ;
+        Y_output = @. validation_settings.Y - μ;
+    end
 
-    # Return output
-    return loss;
+    # Construct estim_settings
+    estim_settings = EstimSettings(Y, Y_output, p, λ, α, β, ε=validation_settings.ε, tol=validation_settings.tol, max_iter=validation_settings.max_iter, prerun=validation_settings.prerun, verb=validation_settings.verb_estim);
+
+    # Estimate penalised VAR
+    kalman_settings = ecm(estim_settings);
+
+    # Run Kalman filter
+    status = KalmanStatus();
+    for t=1:kalman_settings.T
+        kfilter!(kalman_settings, status);
+    end
+
+    # Residuals
+    forecast  = hcat(status.X_prior...)[1:validation_settings.n, :];
+    std_resid = @. ((forecast - Y)/σ)^2;
+
+    # In-sample error
+    if validation_settings.err_type == 1
+        return compute_loss(std_resid)::Float64;
+
+    # Out-of-sample error
+    else
+        return compute_loss(std_resid[:, t0+1:end])::Float64;
+    end
 end
 
+"""
+    compute_loss(std_resid::FloatArray)
+    compute_loss(std_resid::Array{Missing})
+    compute_loss(std_resid::JArray{Float64})
+
+Compute loss function.
+"""
+compute_loss(std_resid::FloatArray) = mean(mean(std_resid, dims=1), dims=2);
+compute_loss(std_resid::Array{Missing}) = NaN;
+compute_loss(std_resid::JArray{Float64}) = mean([mean_skipmissing(std_resid[:,t]) for t=1:size(std_resid,2)]);
 
 """
-    jackknife_err(Y::JArray{Float64,2}, p::Int64, λ::Number, α::Number, β::Number; ajk::Bool=true, subsample::Float64=0.20, max_samples::Int64=500, t0::Int64=1, tol::Float64=1e-3, max_iter::Int64=1000, prerun::Int64=2, verb::Bool=true, demean_Y::Bool=true)
+    jackknife_err(validation_settings::ValidationSettings, p::Int64, λ::Number, α::Number, β::Number)
 
 Return the jackknife out-of-sample error.
 
 # Arguments
-- `Y`: observed measurements (`nxT`), where `n` and `T` are the number of series and observations.
-- `p`: number of lags in the vector autoregression
-- `λ`: overall shrinkage hyper-parameter for the elastic-net penalty
-- `α`: weight associated to the LASSO component of the elastic-net penalty
-- `β`: additional shrinkage for distant lags (p>1)
-- `ajk`: True (false) for the artificial (block) jackknife (default: true)
-- `subsample`: Number of observations removed in the subsampling process, as a percentage of the original sample size. It is bounded between 0 and 1. (default: 0.20)
-- `max_samples`: if `C(T*n,d)` is large, artificial_jackknife would generate `max_samples` jackknife samples. (default: 500 - used only when ajk==true)
-- `t0`: End of the estimation sample (default: 1)
-- `tol`: tolerance used to check convergence (default: 1e-3)
-- `max_iter`: maximum number of iterations for the estimation algorithm (default: 1000)
-- `prerun`: number of iterations prior the actual ECM estimation routine (default: 2)
-- `verb`: verbose output (default: true)
-- `demean_Y`: demean data (default: true)
+- `validation_settings`: ValidationSettings struct
+- `p`: (candidate) number of lags in the vector autoregression
+- `λ`: (candidate) overall shrinkage hyper-parameter for the elastic-net penalty
+- `α`: (candidate) weight associated to the LASSO component of the elastic-net penalty
+- `β`: (candidate) additional shrinkage for distant lags (p>1)
 
 # References
 Pellegrino (2019)
 """
-function jackknife_err(Y::JArray{Float64,2}, p::Int64, λ::Number, α::Number, β::Number; ajk::Bool=true, subsample::Float64=0.20, max_samples::Int64=500, t0::Int64=1, tol::Float64=1e-3, max_iter::Int64=1000, prerun::Int64=2, verb::Bool=true, demean_Y::Bool=true)
+function jackknife_err(validation_settings::ValidationSettings, p::Int64, λ::Number, α::Number, β::Number)
 
     # Block jackknife
-    if ajk == false
-        jackknife_data = block_jackknife(Y, subsample);
+    if validation_settings.err_type == 3
+        jackknife_data = block_jackknife(validation_settings.Y, validation_settings.subsample);
 
     # Artificial jackknife
+    elseif validation_settings.err_type == 4
+        jackknife_data = artificial_jackknife(validation_settings.Y, validation_settings.subsample, validation_settings.max_samples);
+
     else
-        jackknife_data = artificial_jackknife(Y, subsample, max_samples);
+        error("Wrong err_type for jackknife_err!");
     end
 
     # Number of jackknife samples
     samples = size(jackknife_data, 3);
 
     # Compute jackknife loss
-    if verb == true
-        println("jackknife_err > running $samples iterations on $(nworkers()) workers");
-    end
+    verb_message(estim_settings.verb, "jackknife_err > running $samples iterations on $(nworkers()) workers");
 
     loss = 0.0;
     loss = @sync @distributed (+) for j=1:samples
-        fc_err(jackknife_data[:,:,j], p, λ, α, β, iis=false, t0=t0, tol=tol, max_iter=max_iter, prerun=prerun, verb=false, demean_Y=demean_Y)/samples;
+        fc_err(validation_settings, p, λ, α, β; jth_jackknife_data=jackknife_data[:,:,j])/samples;
     end
 
-    if verb == true
-        println("");
-    end
+    verb_message(validation_settings.verb, "");
 
     # Return output
     return loss;
