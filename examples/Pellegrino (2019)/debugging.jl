fda0317023fab101378425ca261accdffc71c787
fipelle <6272230+fipelle@users.noreply.github.com>
2019-07-10 18:08:23 +0100

    Deprecated old methods

--- examples/Pellegrino (2019)/debugging.jl
+++ examples/Pellegrino (2019)/debugging.jl
@@ -1,11 +1,7 @@
 #=
 Tests:
 
-I could use the solution provided on Julia Lang
+- Check formulas in ecm
 - Is ecm_new replacing settings? (not the main ones since the results are correct)
 - Is validation_new replacing settings?
-
-And then focus on the below
-- Is validation_new running without errors?
-- Is validation_new faster / as fast as validation.jl?
 =#
