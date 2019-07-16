e23c794b670df40c718d1b6b30615a1a7fda6db7
fipelle <6272230+fipelle@users.noreply.github.com>
2019-07-16 17:36:41 +0100

    jackknife_err debugged

--- examples/Pellegrino (2019)/debugging.jl
+++ examples/Pellegrino (2019)/debugging.jl
@@ -2,10 +2,5 @@
 Tests:
 
 - Check formulas in ecm
-- Is ecm replacing settings? (not the main ones since the results are correct)
-- Is validation replacing settings?
-- Detailed debugging for validation
-
-TBD:
-- fc_err onwards
+- Debug jackknife_err
 =#
