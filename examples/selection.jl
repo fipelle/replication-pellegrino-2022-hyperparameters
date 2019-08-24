29691f6722efcd6728843836bcaad4962bee404b
fipelle <6272230+fipelle@users.noreply.github.com>
2019-08-24 15:41:04 +0200

    Edits in selection.jl

--- examples/selection.jl
+++ examples/selection.jl
@@ -10,7 +10,16 @@ data_raw = DataFrame(XLSX.readtable("./data/data.xlsx", "weekly_returns")...);
 date = data_raw[3:end,1];
 data = data_raw[3:end,2:end] |> JArray{Float64,2};
 data = permutedims(data);
-n, T = size(data);
+
+# End presample
+end_ps = 416; # 2006-12-29
+
+# End validation sample
+end_vs = 833; # 2014-12-24
+
+# Data used for validation
+data_validation = data[:, 1:end_vs];
+n, T = size(data_validation);
 
 # Select optimal d
 @info "Running optimal_d(n, T)";
@@ -18,7 +27,7 @@ d = optimal_d(n, T);
 
 # Set options for the selection problem
 p_grid=[1, 4]; λ_grid=[1e-4, 4]; α_grid=[0, 1]; β_grid=[1, 4];
-vs = ValidationSettings(4, data, t0=416, subsample=d/(n*T), max_samples=5000, log_folder_path=".");
+vs = ValidationSettings(4, data_validation, t0=end_ps, subsample=d/(n*T), max_samples=5000, log_folder_path=".");
 hg = HyperGrid(p_grid, λ_grid, α_grid, β_grid, 1000);
 
 # Run algorithm
@@ -29,6 +38,9 @@ candidates, errors = select_hyperparameters(vs, hg);
 # Save to file
 save("./res.jld2", Dict("data" => data,
                         "date" => date,
+                        "end_ps" => end_ps,
+                        "end_vs" => end_vs,
+                        "d" => d,
                         "vs" => vs,
                         "hg" => hg,
                         "candidates" => candidates,
